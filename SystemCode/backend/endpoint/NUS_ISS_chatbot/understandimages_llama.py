25af66496a5ea549aaa10699641ea46fb1002aff
LEE KAH WAI <leekahwai@gmail.com>
2023-04-18 23:56:11 +0800

    update and test

--- SystemCode/backend/endpoint/NUS_ISS_chatbot/understandimages_llama.py
+++ SystemCode/backend/endpoint/NUS_ISS_chatbot/understandimages_llama.py
@@ -19,6 +19,8 @@ from llama_index.readers.file.base import (
 from llama_index.indices.query.query_transform.base import (
     ImageOutputQueryTransform,
 )
+image_index = {}
+
 llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003"))
 service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor)
 image_parser = ImageParser(keep_image=True, parse_text=True)
@@ -29,17 +31,10 @@ file_extractor.update(
     ".png": image_parser,
     ".jpeg": image_parser,
 })
-source_folder = 'images'
+source_folder = 'images_llama'
 indexed_folder = 'NUS_ISS_chatbot/data/indexed'
-index_filepath = './text_token_llama.json'
-# add filename as metadata for all documents
-filename_fn = lambda filename: {'file_name': filename}
-image_reader = SimpleDirectoryReader(
-    input_dir=source_folder,
-    file_extractor=file_extractor, 
-    file_metadata=filename_fn,
-    required_exts=['.jpg']
-)
+index_filepath = 'text_token_llama.json'
+
 def move_indexed_files():
     # fetch all files
     for file_name in os.listdir(source_folder):
@@ -64,15 +59,27 @@ def tagImages_llama():
     stop_event = threading.Event()
     dot_printer = threading.Thread(target=print_dots, args=(stop_event,))
     dot_printer.start()
+    # NOTE: we add filename as metadata for all documents
+    # add filename as metadata for all documents
+    filename_fn = lambda filename: {'file_name': filename}
+    image_reader = SimpleDirectoryReader(
+        input_dir='images_llama',
+        file_extractor=file_extractor, 
+        file_metadata=filename_fn,
+    )
     image_documents = image_reader.load_data()
-    image_index = GPTSimpleVectorIndex.load_from_disk(image_documents)
-    for document in image_documents:
-        image_index.insert(document) 
-    image_index.save_to_disk(index_filepath)
+    image_index = GPTSimpleVectorIndex.from_documents(image_documents)
+    response = image_index.query(
+        "Who is Sam",
+        query_transform=ImageOutputQueryTransform(width=600)
+    )
+    print ("Testing: " + str(response))
+        
     elapsed_time = time.time() - start_time
     stop_event.set()
     dot_printer.join()
     print (f"Complete Tagging of images according to LlamaIndex, Elapsed time: {elapsed_time:.4f} seconds")
+    return image_index
 
 def findSuitableImage_llama(search_string, image_index):
     response = image_index.query(
@@ -90,4 +97,5 @@ def findSuitableImage_llama(search_string, image_index):
         return ""
     
 def clearAndLoadTextTokensFromFile_llama():
-    return GPTSimpleVectorIndex.load_from_disk(index_filepath)
+    return tagImages_llama()
+    #return GPTSimpleVectorIndex.load_from_disk('index.json')
